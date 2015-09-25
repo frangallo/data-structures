3ed05857a4f80f97b0cf1c3652b49fd0af889661
frangallo <fgallo@me.com>
2015-09-25 11:50:13 -0700

    finished bst

--- data-structures.rb
+++ data-structures.rb
@@ -53,5 +53,68 @@ class Stack
 
   def first
     @array[@top]
-  end 
+  end
+end
+
+class Node
+  attr_reader :left, :right
+  def initialize(value,left=nil,right=nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+end
+
+
+class BST
+  def initialize
+    @root = nil
+  end
+
+  def insert(node, value)
+    if @root.nil?
+      @root = node
+    elsif node.nil?
+      node = Node.new(value)
+    elsif value <= node.value
+      node.left = insert(node.left,value)
+    elsif value > node.value
+      node.right = insert(node.right,value)
+    end
+    node
+  end
+
+
+  def height(node)
+    return -1 unless @node
+    left = height(node.left)
+    right = height(node.right)
+    [left, right].max + 1
+  end
+
+  def min(node)
+    current = node.left until (node.left).nil?
+    current
+  end
+
+  def max(node)
+    current = node.right until (node.right).nil?
+    current
+  end
+
+  def bfs(node)
+    queue = Queue.new
+    array = []
+    queue.push(node)
+    until queue.empty?
+      node = queue.pop
+      queue.push(node.left)
+      queue.push(node.right)
+      array << node
+    end
+    array  
+  end
+
+
+
 end
